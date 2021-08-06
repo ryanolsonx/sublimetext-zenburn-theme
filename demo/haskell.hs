978c724e0fe2abaf1848fc3f0a50b0d97e7ecbcd
Ryan Olson <ryolson@me.com>
2021-08-06 10:27:57 -0600

    Remove demo files for testing

--- demo/haskell.hs
+++
@@ -1,8 +0,0 @@
-main :: IO ()
-main = do
-    input <- parseInput <$> readFile "input.txt"
-
-    let sizes = [(size .(foldl1 intersection) . (map fromList) . words) line |
-                     line <- input]
-
-    print $ sum sizes
